bc661e3df682fe376c8b7d4d3b5d98ec90e6ecf2
MD Zunaeed Imran <124905281+Zunaeed-Imran@users.noreply.github.com>
2023-04-28 18:20:28 +0600

    Fahrenheite to celsius in JavaScript

--- FahrenheitToCelsius.js
+++ FahrenheitToCelsius.js
@@ -1,3 +1,3 @@
 let fahrenheite = prompt("Enter Fahrenheite Value: ");
 let celsius = (fahrenheite - 32) / 1.8;
-document.write(celsius);
+document.write("the value of Fahrenheite to celsius is: " + fahrenheite + " = " + celsius);
